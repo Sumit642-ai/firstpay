773efa5e0bfec9fd35b77d082edc9ce5d11ac14a
sumitrau <sumitrau74224@gmail.com>
2026-07-02 13:11:12 +0530

    Configure Vercel routing to point to ngrok backend and enable CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -96,12 +96,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5180',
-    'http://localhost:5180',
-    'http://127.0.0.1:5173',
-    'http://localhost:5173',
-]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 SESSION_COOKIE_SAMESITE = 'Lax'
